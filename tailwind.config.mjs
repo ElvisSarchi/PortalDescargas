d94e43c5cff1fda9b7175db59a9b6967aac2edc2
ElvisSarchi <sacianex2@gmail.com>
2023-10-30 13:23:39 -0500

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        saciblue: "#1D2355",
+        sacired: `#E01E26`,
+      },
+    },
+  },
+  plugins: [],
+};

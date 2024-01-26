deae30ec8d9096b82fbab168fa588cf8aa5df728
ElvisSarchi <sacianex2@gmail.com>
2024-01-26 13:05:49 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), compress()],
   output: 'server',
   adapter: node({
     mode: "standalone"
